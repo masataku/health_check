639c8383b1933e864226b7e1f0d04b1ce5a826e6
masataka_omura <masataka.omura@icloud.com>
2021-01-02 19:40:46 +0900

    createアクションの編集

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -8,5 +8,18 @@ class SchoolsController < ApplicationController
   end
   
   def create
+    @school = School.new(school_params)
+    if @school.valid?
+      @school.save
+      redirect_to school_path(@school)
+    else
+      render 'new'
+    end    
+  end
+  
+  private
+
+  def school_params 
+    params.require(:school).permit(:school_name, :email, :password, :password_confirmation, :teacher_password, :student_pasword)
   end  
 end
