fd280db42b568acdca6ac6263feef41e67091f56
masataka_omura <masataka.omura@icloud.com>
2021-01-02 19:44:06 +0900

    バリデーション設定

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,10 @@
 class School < ApplicationRecord
   has_secure_password
+
+  with_options presence: true do
+    validates :school_name
+    validates :email, uniqueness: true
+    validates :teacher_password
+    validates :student_password
+  end  
 end
