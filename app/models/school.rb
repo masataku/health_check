fe201a8c42d5753d8a32c6170175a0d0b4308847
masataka_omura <masataka.omura@icloud.com>
2021-01-06 21:22:35 +0900

    正規表現の修正

--- app/models/school.rb
+++ app/models/school.rb
@@ -3,8 +3,9 @@ class School < ApplicationRecord
 
   with_options presence: true do
     validates :school_name
-    validates :email, uniqueness: true, format: {with: /\A[a-z0-9]{1,}+@{1,1}+[a-z0-9]{1,}\z/, message: 'を入力してください'}
-    with_options format: {with: /\A[a-z0-9]{6,}\z/, message: 'を入力してください'} do
+    validates :email, uniqueness: true, format: {with: /\A[a-z0-9_.-]{1,}+@{1,1}+[a-z0-9-._]{1,}\z/i, message: 'を適切に入力してください'}
+    with_options format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}\z/, message: 'を適切に入力してください'} do
+      validates :password
       validates :teacher_password
       validates :student_password
     end
