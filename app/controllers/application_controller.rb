326abbb1cbb3adad3ec6cafba0ab2a0b9502e05e
masataka_omura <masataka.omura@icloud.com>
2021-01-06 12:08:05 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :set_current_school
 
   def set_current_school
-    @current_school = School.find(session[:school_id])
+    @current_school = School.find_by_id(session[:school_id])
   end  
 end
