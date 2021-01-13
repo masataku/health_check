f12f517db169d6cbe538c54df108e5eb8969a359
masataka_omura <masataka.omura@icloud.com>
2021-01-13 17:50:29 +0900

    createアクション編集

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -8,5 +8,19 @@ class TeachersController < ApplicationController
   end
   
   def create
+    @teacher = Teacher.new(teacher_params)
+    if @teacher.valid? && @teacher.teacher_password == @current_school.teacher_password
+      @teacher.save
+      session[:teacher_id] = @teacher.id
+      redirect_to school_teachers_path
+    else
+      render 'new'
+    end    
+  end
+  
+  private 
+
+  def teacher_params
+    params.require(:teacher).permit(:name, :grade, :my_class, :teacher_password).merge(school_id: @current_school.id)
   end  
 end
