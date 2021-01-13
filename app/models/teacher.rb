858512ce0457723df3f08ca0bb37b4ca296bb978
masataka_omura <masataka.omura@icloud.com>
2021-01-13 18:03:55 +0900

    teacherモデルのバリデーション設定

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,3 +1,10 @@
 class Teacher < ApplicationRecord
   belongs_to :school
+
+  with_options presence: true do
+    validates :name
+    validates :grade, numericality: {only_integer: true, greater_than: 0, less_than: 4}
+    validates :my_class, numericality: {only_integer: true, greater_than: 0}
+    validates :teacher_password
+  end  
 end
