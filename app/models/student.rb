3cf8b02297c8ecf2b8952ffa4acb62dea567d0e5
masataka_omura <masataka.omura@icloud.com>
2021-01-15 18:06:50 +0900

    studentモデルのバリデーション作成

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,18 @@
 class Student < ApplicationRecord
   belongs_to :school
 
-  
+  with_options presence: true do
+    validates :grade, numericality: {only_integer: true, greater_than: 0, less_than: 4}
+    validates :my_class, numericality: {only_integer: true, greater_than: 0}
+    validates :number, numericality: {only_integer: true, greater_than: 0}
+    validates :teacher_password
+    validates :first_name
+    validates :last_name
+    with_options format: {
+      with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,
+      message: "全角カタカナのみで入力して下さい"} do
+      validates :first_name_kana
+      validates :last_name_kana
+    end  
+  end  
 end
