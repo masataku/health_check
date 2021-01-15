9646920df8501d518388b3fe4701f8986d5be51b
masataka_omura <masataka.omura@icloud.com>
2021-01-15 17:14:13 +0900

    ログインしたstudentを識別するメソッド作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :set_current_school
   before_action :set_current_teacher
+  before_action :set_current_student
 
   def set_current_school
     @current_school = School.find_by_id(session[:school_id])
@@ -8,5 +9,9 @@ class ApplicationController < ActionController::Base
 
   def set_current_teacher
     @current_teacher = Teacher.find_by_id(session[:teacher_id])
+  end  
+
+  def set_current_student
+    @current_student = Student.find_by_id(session[:student_id])
   end  
 end
