ff1dd5768fff38c1c6eaa03a01527f9ec1550366
masataka_omura <masataka.omura@icloud.com>
2021-01-15 16:15:41 +0900

    ストロングパラメーター作成

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,11 @@ class StudentsController < ApplicationController
   end
 
   def create
+  end
+  
+  private
+
+  def student_params
+    params.require(:student).permit(:student_password, :first_name, :last_name, :first_name_kana, :last_name_kana, :grade, :my_class, :number)
   end  
 end
