d8e2cd1169f8ac8dc0618e9ee3f5e5fc7c097433
masataka_omura <masataka.omura@icloud.com>
2021-01-17 23:35:10 +0900

    checkのFactoryBot作成

--- spec/factories/checks.rb
+++ spec/factories/checks.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :check do
-    
+    date {Date.today}
+    condition_id {2}
+    symptom_id {1}
+    opinion {"あいうえお"}
+    attend {false}
+    check {true}
+    breakfast {true}
+    bed_time_id {3}
+    leave_early {false}
+    late {false}
+
+    association :student
+    association :sheet
   end
 end
