f7ac2c7636fc7a65216325c66e76c2c93884d3a7
masataka_omura <masataka.omura@icloud.com>
2021-01-17 22:18:24 +0900

    sheet check作成機能作成

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -6,27 +6,33 @@ class ChecksController < ApplicationController
   
   def create
     @check = Check.new(check_params)
+    if @check.save
+      redirect_to school_students_path(@current_student.school)
+    else 
+      render 'new'
+    end    
   end
   
   private
 
   def check_params
-    params.require(:check).permit(:date, :condition_id, :symptoms_id, :opinion, :attend, :check, :breakfast, :bed_time_id, :leave_early, :late).merge(student_id: @current_student.id, sheet_id: todays_sheet_id)
+    params.require(:check).permit(:condition_id, :symptoms_id, :opinion, :attend, :check, :breakfast, :bed_time_id, :leave_early, :late).merge(student_id: @current_student.id, sheet_id: todays_sheet_id, date: Date.today)
   end  
 
   def todays_sheet_id
     if sheet_today = Sheet.find_by(grade: @current_student.grade,
-                                   my_class: @current_student.myclass,
+                                   my_class: @current_student.my_class,
                                    year: @current_student.year,
                                    date: Date.today
                                   )
       return sheet_today.id
     else
-      sheet_today = Sheet.create(grade: @current_student.grade,
-                                 my_class: @current_student.myclass,
+      sheet_today = @current_student.school.sheets.create(
+                                 grade: @current_student.grade,
+                                 my_class: @current_student.my_class,
                                  year: @current_student.year,
                                  date: Date.today
-                                )               
+                                )                                  
       return sheet_today.id  
     end
   end  
