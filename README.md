3403e9f5bbfadc86194dfa214116161974305d9b
masataka_omura <masataka.omura@icloud.com>
2021-01-17 15:09:34 +0900

    sheetsテーブルdate追加

--- README.md
+++ README.md
@@ -182,6 +182,7 @@ https://gyazo.com/baa3ac8aa7ba008cae81d4319bdf6d8a
 | grade    | integer    | null: false                    |
 | my_class | integer    | null: false                    |
 | year     | integer    | null: false                    |
+| date     | date       | null: false                    |
 | school   | references | foreign_key: true, null: false |
 
 ### Association
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_17_053228) do
+ActiveRecord::Schema.define(version: 2021_01_17_060722) do
 
   create_table "schools", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_01_17_053228) do
     t.bigint "school_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "date"
     t.index ["school_id"], name: "index_sheets_on_school_id"
   end
 
