76f3e994152e2e530d199dcac7816ad82c353a96
masataka_omura <masataka.omura@icloud.com>
2021-01-17 23:12:20 +0900

    sheetモデルのバリデーション作成

--- app/models/sheet.rb
+++ app/models/sheet.rb
@@ -1,4 +1,11 @@
 class Sheet < ApplicationRecord
   belongs_to :school
   has_many :checks
+
+  with_options presence: true do
+    validates :grade, numericality: {only_integer: true, greater_than: 0, less_than: 4}
+    validates :my_class, numericality: {only_integer: true, greater_than: 0}
+    validates :year, numericality: {only_integer: true}, length: {is: 4}
+    validates :date 
+  end  
 end
