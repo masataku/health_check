c251b9f9d492bba78a302e7157f564bdf41266b7
masataka_omura <masataka.omura@icloud.com>
2021-01-17 17:47:22 +0900

    ストロングパラメータとsheet作成メソッドの作成

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -4,6 +4,27 @@ class ChecksController < ApplicationController
   end
   
   def create
+  end
+  
+  private
+
+  def check_params
+    params.require(:check).permit(:date, :condition_id, :symptoms_id, :opinion, :attend, :check, :breakfast, :bed_time_id, :temperature_id, :leave_early, :late).merge(student_id: @current_student.id, sheet_id: todays_sheet_id)
+  end  
+
+  def todays_sheet_id
+    if sheet_today = Sheet.find_by(grade: @current_student.grade,
+                                   my_class: @current_student.myclass,
+                                   year: @current_student.year,
+                                   date: Date.today
+                                  )
+      return sheet_today.id
+    else
+      sheet_today = Sheet.create(grade: @current_student.grade,
+                                 my_class: @current_student.myclass,
+                                 year: @current_student.year,
+                                 date: Date.today
+                                )               
       return sheet_today.id  
     end
   end  
