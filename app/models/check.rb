7b9976fea7c39488890a9c61816c9f46d1f806d4
masataka_omura <masataka.omura@icloud.com>
2021-01-17 23:08:49 +0900

    checkモデルのバリデーション設定

--- app/models/check.rb
+++ app/models/check.rb
@@ -3,4 +3,13 @@ class Check < ApplicationRecord
   belongs_to :bed_time
   belongs_to :student
   belongs_to :sheet
+
+  with_options presence: true do
+    validates :date
+    validates :condition_id, numericality: {only_integer: true}
+    validates :attend
+    validates :check
+    validates :breakfast
+    validates :bed_time_id, numericality: {only_integer: true, other_than: 1}
+  end  
 end
