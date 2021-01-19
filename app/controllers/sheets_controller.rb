5bdff833f00cf909804f5b5c9d59db76ad4831ae
masataka_omura <masataka.omura@icloud.com>
2021-01-19 18:22:37 +0900

    sheet詳細にstudentを一覧表示

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -1,4 +1,6 @@
 class SheetsController < ApplicationController
   def show
+    @sheet = Sheet.find(params[:id])
+    @students = Student.where(grade: @sheet.grade, my_class: @sheet.my_class, year: @sheet.year, school_id: @sheet.school.id).order(number: :asc)
   end
 end
