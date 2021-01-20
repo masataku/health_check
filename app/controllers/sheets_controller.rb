1e479e06b5d1e0dbd8acaadaa2a2b1877efcaa38
masataka_omura <masataka.omura@icloud.com>
2021-01-20 13:30:44 +0900

    sheet詳細ページのcheck一覧の雛形作成

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -2,5 +2,6 @@ class SheetsController < ApplicationController
   def show
     @sheet = Sheet.find(params[:id])
     @students = Student.where(grade: @sheet.grade, my_class: @sheet.my_class, year: @sheet.year, school_id: @sheet.school.id).order(number: :asc)
+    @checks = @sheet.checks
   end
 end
