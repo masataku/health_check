ce2a1ccd58531ff35bf22952335257d0069b61d2
masataka_omura <masataka.omura@icloud.com>
2021-01-20 15:18:06 +0900

    fiscal_yearをapplication.controllerへ移動
    teacherにもyearカラムもたせるため

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -32,16 +32,7 @@ class StudentsController < ApplicationController
 
   def student_params
     params.require(:student).permit(:student_password, :first_name, :last_name, :first_name_kana, :last_name_kana, :grade, :my_class, :number).merge(school_id: params[:school_id], year: fiscal_year)
-  end  
-
-  def fiscal_year
-    fiscal_year = Date.today.year
-    if Date.today.month == 1..3
-      return fiscal_year - 1
-    else
-      return fiscal_year
-    end    
-  end  
+  end 
 
   def is_fiscal_year? 
     if @current_student != nil
