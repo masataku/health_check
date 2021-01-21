e9e1b0a4302b94c502885be49273a960052d13d4
masataka_omura <masataka.omura@icloud.com>
2021-01-21 16:01:44 +0900

    バリデーションの追加

--- app/models/school.rb
+++ app/models/school.rb
@@ -7,6 +7,7 @@ class School < ApplicationRecord
   with_options presence: true do
     validates :school_name
     validates :email, uniqueness: true, format: {with: /\A[a-z0-9_.-]{1,}+@{1,1}+[a-z0-9-._]{1,}\z/i, message: 'を適切に入力してください'}
+    validates :password_confirmation
     with_options format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}\z/, message: 'を適切に入力してください'} do
       validates :password
       validates :teacher_password
