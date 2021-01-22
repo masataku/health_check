32d480077d09910f42d1f2b45e1a93341a684b80
masataka_omura <masataka.omura@icloud.com>
2021-01-22 15:28:04 +0900

    schoolにカラム追加
    showページで校長の名前を表示するため

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_20_062034) do
+ActiveRecord::Schema.define(version: 2021_01_22_062334) do
 
   create_table "check_symptoms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "check_id", null: false
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2021_01_20_062034) do
     t.string "student_password", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "head_teacher", null: false
   end
 
   create_table "sheets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
