3dba76ab69a7843c56d9a339dc46b4cb72a21d76
masataka_omura <masataka.omura@icloud.com>
2021-01-29 19:30:32 +0900

    teachersコントローラーの単体テスト作成

--- spec/requests/teachers_request_spec.rb
+++ spec/requests/teachers_request_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Teachers", type: :request do
-
+  before do
+    @school = FactoryBot.create(:school)
+  end  
+  describe "GET #new" do
+    it "newアクションにリクエストすると正常にレスポンスが返ってくる" do
+      get new_school_teacher_path(@school)
+      expect(response.status).to eq 200
+    end
+    it "newアクションにリクエストすると教員作成ボタンがある" do
+      get new_school_teacher_path(@school)
+      expect(response.body).to include "教員として入る"
+    end
+  end  
 end
