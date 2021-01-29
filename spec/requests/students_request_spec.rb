83adc545b80be23d8d6f7a40a227b09fd0b0c38d
masataka_omura <masataka.omura@icloud.com>
2021-01-29 19:33:54 +0900

    studentコントローラの単体テスト作成

--- spec/requests/students_request_spec.rb
+++ spec/requests/students_request_spec.rb
@@ -1,12 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Students", type: :request do
-
-  describe "GET /new" do
-    it "returns http success" do
-      get "/students/new"
-      expect(response).to have_http_status(:success)
+  before do
+    @school = FactoryBot.create(:school)
+  end  
+  describe "GET #new" do
+    it "newアクションにリクエストすると正常にレスポンスが返ってくる" do
+      get new_school_student_path(@school)
+      expect(response.status).to eq 200
     end
-  end
-
+    it "newアクションにリクエストすると生徒作成ボタンがある" do
+      get new_school_student_path(@school)
+      expect(response.body).to include "生徒登録する"
+    end
+  end  
 end
