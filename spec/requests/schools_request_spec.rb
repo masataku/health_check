91bc7a15e5afefade40021d405ddc37f2300d115
masataka_omura <masataka.omura@icloud.com>
2021-02-09 19:13:00 +0900

    Update schools_request_spec.rb

--- spec/requests/schools_request_spec.rb
+++ spec/requests/schools_request_spec.rb
@@ -10,11 +10,11 @@ RSpec.describe "Schools", type: :request do
     it "indexアクションにリクエストすると学校新規作成リンクが表示されている" do
       get root_path
       expect(response.body).to include "今すぐはじめる"
-      expect(response.body).to include "新規学校作成"
+      expect(response.body).to include "新規学校登録"
     end  
     it "indexアクションにリクエストするとがログインリンクが表示されている" do
       get root_path
-      expect(response.body).to include "自分の学校へ行く"
+      expect(response.body).to include "ログイン"
     end  
 
     it "indexアクションにリクエストすると現在全国で参加している学校数が表示されている" do
@@ -33,9 +33,9 @@ RSpec.describe "Schools", type: :request do
       get new_school_path
       expect(response.status).to eq 200
     end  
-    it "newアクションにリクエストすると学校新規作成リンクが表示されている" do
+    it "newアクションにリクエストすると学校新規登録リンクが表示されている" do
       get new_school_path
-      expect(response.body).to include "新規学校作成"
+      expect(response.body).to include "新規学校登録"
     end  
     it "newアクションにリクエストするとがログインリンクが表示されている" do
       get new_school_path
@@ -53,13 +53,13 @@ RSpec.describe "Schools", type: :request do
       get login_form_schools_path
       expect(response.status).to eq 200
     end  
-    it "login_formアクションにリクエストすると学校新規作成リンクが表示されている" do
+    it "login_formアクションにリクエストすると学校新規登録リンクが表示されている" do
       get login_form_schools_path
-      expect(response.body).to include "新規学校作成"
+      expect(response.body).to include "新規学校登録"
     end  
     it "login_formアクションにリクエストするとがログインリンクが表示されている" do
       get login_form_schools_path
-      expect(response.body).to include "自分の学校へ行く"
+      expect(response.body).to include "ログイン"
     end  
 
     it "login_formアクションにリクエストするとログインボタンがある" do
